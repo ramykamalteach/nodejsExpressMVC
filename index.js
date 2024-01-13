1e8e849542f84ea08a0a9f7f61dbe9eb026c5adb
ramy.kamal.teachx <76243913+ramyyat@users.noreply.github.com>
2024-01-13 19:35:53 +0200

    phase 002 - web view

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send("welcome to our Home page");
 });
 app.get('/about', (req, res) => {
     res.send("About our Company");
+});
+app.get('/x', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/contacts.html'));
 });
 
 port = 3000;
