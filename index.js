04852707a01235cd0ba50b5e921e282bbafbbb4d
ramy.kamal.teachx <76243913+ramyyat@users.noreply.github.com>
2024-01-13 22:06:06 +0200

    send variables and arrays to EJS view

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ app.get('/contacts', (req, res) => {
     res.render('pages/contacts');
 });
 
+app.get('/staff', (req, res) => {
+    var staffTitle = "Mean Full Stack Staff";
+    var staffCount = 15;
+    var emps = [
+        { name: "Ali Reda", job: "frontend" },
+        { name: "Waleed Mohammed", job: "backtend", address: "mansoura" },
+        { name: "FAthy Farouk", job: "AI-dev" },
+    ];
+    res.render('pages/staff', {
+        depName: staffTitle,
+        staffCount: staffCount,
+        emps: emps
+    });
+});
+
+
+
+/* ---------------- start server --------------- */
 port = 3000;
 app.listen(port, () => {
     //
